4215b26abe43710a215963521a37d300daf7f44c
sanynote <sunnyjuice94@gmail.com>
2023-05-09 16:24:45 +0900

    feat:firebase key env 설정

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: 'sss',
-  authDomain: "qqqq",
-  projectId: "qqqq",
-  storageBucket: "qqqq",
-  messagingSenderId: "qqqq",
-  appId: "qqqq"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
