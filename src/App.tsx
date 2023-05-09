ac830b72cf9cb6e77fb464e4226f222ad716a674
sanynote <sunnyjuice94@gmail.com>
2023-05-09 14:12:37 +0900

    feat:회원가입,로그인 화면,로직 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
+import {authFire} from "./firebase";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("　");
+  const [user,setUser] = useState({})
+
+  const signInButton = async() => {
+    try {
+      const curUserInfo = await signInWithEmailAndPassword(authFire, email, password);
+      console.log(curUserInfo.user,'ssss');
+      setUser(curUserInfo.user);
+    } catch (err: any) {
+      console.log(err.code,'eeee');
+      /*
+      입력한 아이디가 없을 경우 : auth/user-not-found.
+      비밀번호가 잘못된 경우 : auth/wrong-password.
+      */
+    }
+  }
+
+  const signUpButton = async() => {
+    try {
+      setErrorMsg('　');
+      const createdUser = await createUserWithEmailAndPassword(authFire, email, password);
+      console.log(createdUser, '가입 성공');
+      setEmail("");
+      setPassword("");
+
+    } catch(err:any){
+      console.log(err.code, errorMsg);
+      switch (err.code) {
+        case 'auth/weak-password':
+          setErrorMsg('비밀번호는 6자리 이상이어야 합니다');
+          break;
+        case 'auth/invalid-email':
+          setErrorMsg('잘못된 이메일 주소입니다');
+          break;
+        case 'auth/email-already-in-use':
+          setErrorMsg('이미 가입되어 있는 계정입니다');
+          break;
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="backArea">
+      <form>
+        <h2>
+        gmail로 로그인
+        </h2>
+        <div>
+
+        <label>g-mail</label><input type='email' />
+        </div>
+        <div>
+
+        <label>password</label><input type='password' />
+        </div>
+        <button>로그인</button>
+      </form>
+      <div>
+        <h2>
+          email로 회원가입 or 로그인
+        </h2>
+        <div>
+          <label>e-mail</label><input type='email' value={email} onChange={e=>setEmail(e.target.value)}/>
+        </div>
+        <div>
+          <label>password</label><input type='password' value={password} onChange={e=>setPassword(e.target.value)}/>
+        </div>
+        <button type = "submit" onClick={()=>signInButton()}>로그인</button>
+        <button type="submit" onClick={()=>signUpButton()}>회원가입하기</button>
+      </div>
+
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebase from "./firebase";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-console.log(firebase,'firebase')
 
 root.render(
   <React.StrictMode>
