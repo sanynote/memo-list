f81e2f3ae575f307658b65a9949362fcf4f33ef2
sanynote <sunnyjuice94@gmail.com>
2023-05-09 13:56:50 +0900

    feat:firebase 설치, 파일생성

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "qqqq",
+  authDomain: "qqqq",
+  projectId: "qqqq",
+  storageBucket: "qqqq",
+  messagingSenderId: "qqqq",
+  appId: "qqqq"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "./firebase";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+console.log(firebase,'firebase')
+
 root.render(
   <React.StrictMode>
     <App />
