75230efa17f5ea507d7cf5679bd45c677dca6ba5
sanynote <sunnyjuice94@gmail.com>
2023-05-12 17:50:42 +0900

    feat: update 추가

--- src/pages/memo/memo.detail.tsx
+++ src/pages/memo/memo.detail.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {collection, doc, getDoc, deleteDoc} from "firebase/firestore";
+import {collection, doc, getDoc, deleteDoc, updateDoc} from "firebase/firestore";
 import {db} from "../../firebase";
 import {useLocation, useNavigate} from "react-router-dom";
 import BackButton from "../../common/back.button";
@@ -32,7 +32,7 @@ function MemoDetail() {
 
   const getDetailMemo = async () => {
     try {
-      const docRef = doc(db, uid, '1v6QWbKAF95f9iL7psOv');
+      const docRef = doc(db, uid, 'qrQkqowtP7BdtKEXEnVG');
       const memoData = await getDoc(docRef);
       const memoDetail = memoData.data()
       setMemoTitle(memoDetail!['title'])
@@ -44,6 +44,19 @@ function MemoDetail() {
   }
 
   console.log(memoTitle, memoContents)
+
+  const updateMemo = async ()=>{
+    try{
+
+      const updateMemo = doc(db, uid, test);
+      await updateDoc(updateMemo,{title:memoTitle,contents:memoContents});
+      navigate(`/list`)
+      console.log('메모 수정 성공')
+
+    }catch (e) {
+      console.log(e, '메모 수정 실패')
+    }
+  }
 
   const removeMemo = async () => {
     try {
@@ -60,8 +73,11 @@ function MemoDetail() {
     <div>
       <BackButton/>
       <div>디테일페이지</div>
-      <div>{memoTitle}</div>
-      <div>{memoContents}</div>
+      <input value={memoTitle} onChange={(e)=>setMemoTitle(e.target.value)}/>
+      <input value={memoContents} onChange={(e)=>setMemoContents(e.target.value)}/>
+      {/*<div>{memoTitle}</div>*/}
+      {/*<div>{memoContents}</div>*/}
+      <div onClick={() => updateMemo()}>메모 수정 버튼</div>
       <div onClick={() => removeMemo()}>메모 삭제 버튼</div>
     </div>
   );
