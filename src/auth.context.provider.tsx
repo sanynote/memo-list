5a3baef4a1049e5c50e8132260a34045866538fe
sanynote <sunnyjuice94@gmail.com>
2023-05-23 13:33:31 +0900

    fix:any 수정

--- src/auth.context.provider.tsx
+++ src/auth.context.provider.tsx
@@ -1,10 +1,14 @@
-import React, {createContext} from 'react';
+import React, {createContext, ReactNode} from 'react';
 import {onAuthStateChanged} from "firebase/auth";
 import {authFire} from "./firebase";
 
 export const AuthContext = createContext(false);
 
-export const AuthContextProvider = ({children}: any) => {
+interface Props {
+  children:ReactNode
+}
+
+export const AuthContextProvider = ({children} : Props) => {
   const uidCheck = localStorage.getItem('uid') ? true : false
   const [isLoggedIn, setIsLoggedIn] = React.useState(uidCheck);
   React.useEffect(() => {
